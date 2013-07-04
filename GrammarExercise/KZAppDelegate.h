59a11d43b5301a22c6a3054c5270e93897960b07
shangde.Jim <shangde@hoho.local>
2013-07-04 12:45:20 +0800

    Initial Commit

---
+++ GrammarExercise/KZAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  KZAppDelegate.h
+//  GrammarExercise
+//
+//  Created by shangde.Jim on 13-7-4.
+//  Copyright (c) 2013年 Kylinzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KZAppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ GrammarExercise/KZFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  KZFirstViewController.h
+//  GrammarExercise
+//
+//  Created by shangde.Jim on 13-7-4.
+//  Copyright (c) 2013年 Kylinzhou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KZFirstViewController : UIViewController
+
+@end
